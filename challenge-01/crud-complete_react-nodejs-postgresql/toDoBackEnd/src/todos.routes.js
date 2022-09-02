876cc31978d24a648d18c082652892d3e95d319e
brmarmello <brmarmello@gmail.com>
2022-09-01 23:15:02 -0300

    feat: ends read route.

--- challenge-01/crud-complete_react-nodejs-postgresql/toDoBackEnd/src/todos.routes.js
+++ challenge-01/crud-complete_react-nodejs-postgresql/toDoBackEnd/src/todos.routes.js
@@ -8,10 +8,12 @@ toDosRoutes.post("/todos", (request, response) => {
   const { name } = request.body;
   allTodos.push({ name, status: false });
   return response.status(201).json(allTodos);
-})
+});
 
 // R
-
+toDosRoutes.get("/todos", (request, response) => {
+  return response.status(200).json(allTodos);
+});
 
 // U
 
