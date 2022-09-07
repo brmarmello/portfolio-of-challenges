860c7a7db46d0785c8e867cf0f235a5ad06500c2
brmarmello <brmarmello@gmail.com>
2022-09-07 00:25:47 -0300

    dev: create array for tasks.

--- challenge-01/crud-complete_react-nodejs-postgresql/toDoFrontEnd/to-do-list/src/App.js
+++ challenge-01/crud-complete_react-nodejs-postgresql/toDoFrontEnd/to-do-list/src/App.js
@@ -1,11 +1,41 @@
 // import logo from './logo.svg';
 import './App.css';
 
+const arrayTodos = [
+  { name: "Limpar a Casa", status: false },
+  { name: "Limpar o Cachorro", status: false }
+];
+
+const Todos = ({ todos }) => {
+    return (
+      <div className="todos">
+        {todos.map((todo) => {
+          return (
+            <div className="todo">
+              {/* <button
+                onClick={() => modifyStatusTodo(todo)}
+                className="checkbox"
+                style={{ backgroundColor: todo.status ? "#A879E6" : "white" }}
+              ></button> */}
+              <p>{todo.name}</p>
+              {/* <button onClick={() => handleWithEditButtonClick(todo)}>
+                <AiOutlineEdit size={20} color={"#64697b"}></AiOutlineEdit>
+              </button>
+              <button onClick={() => deleteTodo(todo)}>
+                <AiOutlineDelete size={20} color={"#64697b"}></AiOutlineDelete>
+              </button> */}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="container">
+        <Todos todos={arrayTodos}></Todos>
       </header>
     </div>
   );
